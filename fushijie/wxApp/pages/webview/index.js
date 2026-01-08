f1b6cada3b5a1298b8d865da93e90291fa5780ab
aidegl <aidegl666@gmail.com>
2026-01-08 09:45:17 +0800

    111

--- fushijie/wxApp/pages/webview/index.js
+++ fushijie/wxApp/pages/webview/index.js
@@ -9,10 +9,13 @@ Page({
   onLoad() {
     // 1. 在页面加载时初始化基础URL（只生成一次版本号，防止onShow时刷新）
     const BASE_URL = 'https://100000whys.cn/project/fushijie/webview/dist/index.html?item=c10c60ae-78e5-48da-9401-aa8d3e3908f4';
+<<<<<<< Updated upstream
 <<<<<<< HEAD
 
 =======
 >>>>>>> parent of 6aa0550 (Merge branch 'main' of https://github.com/aidegl/item)
+=======
+>>>>>>> Stashed changes
 
     let rawBaseUrl = BASE_URL;
     console.log('[Webview] onLoad rawBaseUrl:', rawBaseUrl);
