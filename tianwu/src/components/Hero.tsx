a2250f046b5a4f48896d6b2d620b86af1c9cd1e2
aidegl <aidegl666@gmail.com>
2026-01-12 22:22:57 +0800

    111

--- tianwu/src/components/Hero.tsx
+++ tianwu/src/components/Hero.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { motion } from 'framer-motion';
-import logo from '../assets/logo.png';
+import logo from '../assets/img/天悟2.png';
 
 const Hero = () => {
   return (
