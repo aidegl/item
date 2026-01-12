ec5e004de5e959a2dffe11e9a466537249deaea1
aidegl <aidegl666@gmail.com>
2026-01-12 22:27:48 +0800

    1211

--- tianwu/vite.config.ts
+++ tianwu/vite.config.ts
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // 改为相对路径，确保在任何目录下直接打开 index.html 都能找到资源
 })
