aeaf3f9705e8c4b73fdbf72931e4a8f63a7bd642
aidegl <aidegl666@gmail.com>
2026-02-16 17:05:36 +0800

    11

--- fsj/merchantDashboard/wxApp/app.js
+++ fsj/merchantDashboard/wxApp/app.js
@@ -1,9 +1,10 @@
-const MingDaoYunArrayAPI = require('./utils/MingdaoYunArrayAPI');
+const { initMerchantData } = require('./utils/initMerchant');
 
 App({
   onLaunch() {
     console.log('小程序启动');
-    this.initMerchantData();
+    const merchantId = this.globalData.merchantId;
+    this.initMerchantData(merchantId);
   },
 
   onShow() {
@@ -16,45 +17,14 @@ App({
 
   globalData: {
     userInfo: null,
-    merchantData: null
+    merchantData: null,
+    merchantId: '{商家ID}'
   },
 
-  async initMerchantData() {
-    try {
-      const api = new MingDaoYunArrayAPI();
-
-      const filters = [
-        {
-          "controlId": "mid",
-          "dataType": 2,
-          "spliceType": 1,
-          "filterType": 2,
-          "value": "{商家ID}"
-        },
-        {
-          "controlId": "use",
-          "dataType": 2,
-          "spliceType": 1,
-          "filterType": 2,
-          "value": "1"
-        }
-      ];
-
-      const result = await api.getData({
-        worksheetId: 'scgl',
-        filters: filters,
-        pageSize: 50,
-        pageIndex: 1
-      });
-
-      if (result.success) {
-        this.globalData.merchantData = result.data;
-        console.log('商家数据加载成功：', result.data);
-      } else {
-        console.error('商家数据加载失败：', result.error_msg);
-      }
-    } catch (error) {
-      console.error('初始化商家数据异常：', error);
+  async initMerchantData(merchantId) {
+    const data = await initMerchantData(merchantId);
+    if (data) {
+      this.globalData.merchantData = data;
     }
   }
 });
