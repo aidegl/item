4eb53abd874927e059fbaff018d84c53b3c5c191
aidegl <aidegl666@gmail.com>
2026-02-18 20:28:23 +0800

    111

--- fsj/merchantDashboard/components/carousel.js
+++ fsj/merchantDashboard/components/carousel.js
@@ -4,7 +4,7 @@ module.exports = {
 
   generateHTML(component) {
     return `  <swiper class="carousel" indicator-dots autoplay interval="3000">
-    ${component.componentItems.map(item => `    <swiper-item><image src="${item}" mode="aspectFill"></image></swiper-item>`).join('\n')}
+    ${component.componentItems.map(item => `    <swiper-item><image src="${item.url}" mode="aspectFill"></image></swiper-item>`).join('\n')}
   </swiper>`;
   },
 
@@ -29,11 +29,7 @@ module.exports = {
   },
 
   getDefaultItems() {
-    return [
-      'https://via.placeholder.com/750x400/667eea/ffffff?text=Slide+1',
-      'https://via.placeholder.com/750x400/667eea/ffffff?text=Slide+2',
-      'https://via.placeholder.com/750x400/667eea/ffffff?text=Slide+3'
-    ];
+    return [];
   },
 
   getPropertyLabels() {
