27a85bbece881bd8cdbba5148197d1a950f57362
aidegl <aidegl666@gmail.com>
2026-02-22 10:41:41 +0800

    111

--- fsj/merchantDashboard/components/function-list.js
+++ fsj/merchantDashboard/components/function-list.js
@@ -32,7 +32,7 @@ module.exports = {
   generateHTML(component) {
     const dataKey = component.dataKey || 'functionList';
     return `  <view class="function-list">
-    <view class="function-grid grid-{{functionList.length >= 9 ? 5 : (functionList.length >= 6 ? 3 : 4)}}">
+    <view class="function-grid {{functionList.length >= 9 ? 'grid-5' : (functionList.length >= 7 ? 'grid-4' : (functionList.length >= 6 ? 'grid-3' : 'grid-auto'))}}">
       <block wx:for="{{${dataKey}}}" wx:key="name">
         <view class="function-item"><image src="{{item.icon}}" mode="aspectFit"></image><text>{{item.name}}</text></view>
       </block>
@@ -49,6 +49,10 @@ module.exports = {
 .function-grid {
   display: grid;
   gap: 0;
+}
+
+.grid-auto {
+  grid-template-columns: repeat(auto-fit, minmax(25%, 1fr));
 }
 
 .grid-4 {
