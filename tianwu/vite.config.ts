b6d37dd34a8bc471dfbe7ecb30877193f3b6003f
aidegl <aidegl666@gmail.com>
2026-05-01 14:03:55 +0800

    Delete tianwu directory

--- tianwu/vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: './', // 改为相对路径，确保在任何目录下直接打开 index.html 都能找到资源
-})
